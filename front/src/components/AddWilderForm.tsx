cfe523d2fa317c18a53fd467fa1f84ab40794736
AdrienSergentWildCodeSchool <adrien.sergent@wildcodeschool.com>
2023-06-02 07:56:02 +0200

    add wilder mutation front

--- front/src/components/AddWilderForm.tsx
+++ front/src/components/AddWilderForm.tsx
@@ -1,29 +1,44 @@
-import axios from "axios";
 import { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+
 const AddWilderForm = () => {
   const [wilderName, setWilderName] = useState("");
+
+  const CREATE_WILDER = gql`
+    mutation createWilder($name: String!) {
+      createWilder(name: $name) {
+        id
+        name
+      }
+    }
+  `;
+
+  const [createWilder, { loading, error }] = useMutation(CREATE_WILDER);
+
+  const handleAddWilder = async () => {
+    try {
+      const { data } = await createWilder({
+        variables: { name: wilderName },
+      });
+      console.log("Wilder added:", data.createWilder);
+      // Faites quelque chose avec les données de la mutation si nécessaire
+    } catch (error) {
+      console.error("Error adding wilder:", error);
+      // Gérez l'erreur de la mutation ici
+    }
+  };
+
   return (
     <div>
       <input
         value={wilderName}
-        onChange={(e) => {
-          setWilderName(e.target.value);
-        }}
+        onChange={(e) => setWilderName(e.target.value)}
       />
       <br />
-      <button
-        onClick={async () => {
-          try {
-            await axios.post("http://localhost:5000/api/wilder", {
-              name: wilderName,
-            });
-          } catch (err) {
-            console.log(err);
-          }
-        }}
-      >
+      <button onClick={handleAddWilder} disabled={loading}>
         Save Wilder
       </button>
+      {error && <p>Error: {error.message}</p>}
     </div>
   );
 };
